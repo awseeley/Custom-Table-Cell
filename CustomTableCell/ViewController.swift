6ea23ddc48562d3da0d07d6f4c85bc1e180169e0
Andrew Seeley <awseeley@gmail.com>
2014-10-05 23:09:43 +1100

    Custom Table Cell
    Initial Commit

--- CustomTableCell/ViewController.swift
+++ CustomTableCell/ViewController.swift
@@ -1,25 +1,51 @@
-//
-//  ViewController.swift
-//  CustomTableCell
-//
-//  Created by Andrew Seeley on 5/10/2014.
-//  Copyright (c) 2014 Seemu. All rights reserved.
-//
+
+
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+    var carNames: [String] = ["Ferrari", "BMW", "Mitsubishi", "Lambo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableData.append("Seemu")
+        self.tableView.reloadData()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.carNames.count
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell 	{
+        
+        var cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = self.carNames[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("Row \(indexPath.row) selected")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
