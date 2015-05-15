0ed7a4840d1c23fc426f171fa5e3dbd6e8fa9723
Andrew Seeley <awseeley@gmail.com>
2015-05-15 21:15:05 +1000

    Updated for Swift 1.2 with XCode 6.3
    Updated for Swift 1.2 with XCode 6.3

--- CustomTableCell/ViewController.swift
+++ CustomTableCell/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var tableData: [String] = ["Ferrari", "BMW", "Mitsubishi", "Lambo"]
     
+    // 1
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,28 +19,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Register custom cell
         var nib = UINib(nibName: "vwTblCell", bundle: nil)
         tableView.registerNib(nib, forCellReuseIdentifier: "cell")
-
+        
     }
     
+    // 2
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.tableData.count
     }
     
+    
+    // 3
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell 	{
         
-        var cell:TblCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as TblCell
+        var cell:TblCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! TblCell
         
         cell.lblCarName.text = tableData[indexPath.row]
         cell.imgCarName.image = UIImage(named: tableData[indexPath.row])
         
         return cell
-
+        
     }
     
-    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+    // 4
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         println("Row \(indexPath.row) selected")
     }
     
+    // 5
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 70
     }
